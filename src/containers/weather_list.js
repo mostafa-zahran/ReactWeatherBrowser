8ff44f1ca0da0c1489ce1824e12f709430bd0c3d
Mostafa Zahran <mostafa.k.zahran@gmail.com>
2017-09-14 11:24:24 +0300

    add charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,13 +3,20 @@
  */
 import React, {Component} from 'react'
 import WeatherReducerToWeatherListWire from '../connections/weather_reducer_to_weather_list'
+import Chart from '../components/chart';
 
 export class WeatherList extends Component {
     renderWeather(cityData){
         const name = cityData.city.name;
+        const temps = cityData.list.map((weather)=> weather.main.temp);
+        const humidities = cityData.list.map((weather)=> weather.main.humidity);
+        const pressures = cityData.list.map((weather)=> weather.main.pressure);
         return(
             <tr key={name}>
                 <td>{name}</td>
+                <td><Chart data={temps} color="orange" units="K"/></td>
+                <td><Chart data={pressures} color="black" units="hPa"/></td>
+                <td><Chart data={humidities} color="green" units="%"/></td>
             </tr>
         );
     }
@@ -19,9 +26,9 @@ export class WeatherList extends Component {
             <thead>
             <tr>
                 <th>City</th>
-                <th>Temperature</th>
-                <th>Pressure</th>
-                <th>Humidity</th>
+                <th>Temperature (K)</th>
+                <th>Pressure (hPa)</th>
+                <th>Humidity (%)</th>
             </tr>
             </thead>
             <tbody>
